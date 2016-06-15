0aaece27c30db629ffc813b7f250301c95f3cfc3
gaba <gaba@riseup.net>
2016-06-15 02:58:14 +0100

    Old files that I used before in the other report.

--- data/20160522/evaluate_level.py
+++ data/20160522/evaluate_level.py
@@ -4,21 +4,8 @@
 
 import pymongo, json
 
-basic_fields = { "buyer" : ["address", "name"],
-                "tender" : [ {"tenderers": [ "identifier" ] }, "title", "description",
-                             "status", "procurementMethod", "awardPeriod",
-                             {"items": ["description", "quantity"]}, "documents"
-                            ],
-                "award" : [ "id", "title", "description", "date", "value",
-                            { "supplier": [ "name" , "address"]}
-                          ],
-                "contract" : [ "id", "title", "status", "period", "value",
-                               { "items": ["description", "quantity"]}
-                             ]
-                }
-
-def level_basic(release):
-    basic_values_file = 'basic.json'
+def level_basic(basic_fields, release):
+    basic_values_file = 'intermediate.json'
     basic_values = {}
     total_fields = 0
     completed_fields = 0
@@ -33,15 +20,32 @@ def level_basic(release):
                                 if type(release[i][p]) is list:
                                     total_fields = total_fields + 1
                                     found = False
+                                    if type(j) is str:
+                                        for x in release[i][p]:
+                                            if x[j]:
+                                                found = True
+                                                completed_fields = completed_fields + 1
+                                                k = i + '.' + p + '.' + j
+                                                basic_values[k] = x[j]
+                                                break
+                                    if type(j) is dict:
+                                        for m in j:
+                                            for x in release[i][p]:
+                                                if x[m]:
+                                                    print x[m]
+                                                    found = True
+                                                    completed_fields = completed_fields + 1
+                                                    k = i + '.' + p + '.' + m
+                                                    basic_values[k] = x[m]
+                                                    break
+                                    if not found :
+                                        missing_fields.append(j)
+                                if type(release[i][p]) is dict:
                                     for x in release[i][p]:
-                                        if x[j]:
-                                            found = True
+                                        if x == j:
                                             completed_fields = completed_fields + 1
                                             k = i + '.' + p + '.' + j
-                                            basic_values[k] = x[j]
-                                            break
-                                    if not found :
-                                        missing_fields.append("Field not found %s" % j)
+                                            basic_values[k] = release[i][p][x]
                                 else:
                                     print "YEAH???"
                 else: # for example "title"
@@ -70,10 +74,14 @@ def main():
 
     contracts = collection.find()
 
+
+    with open('intermediate_fields.json', 'r') as outfile:
+        basic_fields = json.load(outfile)
+
     for c in contracts:
         for release in c['releases']:
             # check level of completeness for basic level
-            basic, missing_fields = level_basic(release)
+            basic, missing_fields = level_basic(basic_fields, release)
             print 'Basic Level: %8.2f complete' % basic
             for m in missing_fields:
                 print '\t Missing field: %s' % m
